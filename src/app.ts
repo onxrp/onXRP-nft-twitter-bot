3ed5536edb318504800df547baae91ad34f94f79
dmytrobaida <dmytrobaida@outlook.com>
2022-11-10 16:18:35 +0200

    adds resubscribe functionality

--- src/app.ts
+++ src/app.ts
@@ -6,48 +6,59 @@ import { tokenAcceptOfferHandler } from "./handlers/tokenAcceptOfferHandler";
 import { XrpServer } from "./configuration";
 import { log } from "./utils/logger";
 import { ValidTransactions } from "./constants";
-import { TwitJsClient, TwitterApiV2Client } from "./utils/twitterClient";
+import { TwitterApiV2Client } from "./utils/twitterClient";
+
+let intervalTimer: NodeJS.Timer;
+let checkNumber = 0;
 
 export async function runApplication() {
-    log("Started listening for XRP transactions!");
+    try {
+        log("Started listening for XRP transactions!");
 
-    const client = new Client(XrpServer);
-    await client.connect();
-    
-    // const twitterClient = new TwitJsClient();
-    const twitterClient = new TwitterApiV2Client();
+        const client = new Client(XrpServer);
+        await client.connect();
 
-    log(`Successfully connected to XRP server ${XrpServer}!`);
+        const twitterClient = new TwitterApiV2Client();
 
-    const subscribe: SubscribeRequest = {
-        id: "Check all transactions in XRP",
-        command: "subscribe",
-        streams: ["transactions"],
-    }
+        log(`Successfully connected to XRP server ${XrpServer}!`);
+        log("Starting interval to subscribe for XRP events!");
 
-    await client.request(subscribe);
+        intervalTimer = setInterval(async () => {
+            await client.request({
+                id: `Check all transactions in XRP №${checkNumber++}`,
+                command: "subscribe",
+                streams: ["transactions"],
+            });
 
-    log("Subscribed for updates from XRP!");
+            log("Subscribed for updates from XRP!");
+        }, 600000); // 10 minutes
 
-    client.on("transaction", async tx => {
-        const transactionType = tx?.transaction?.TransactionType;
-        if (transactionType != null && ValidTransactions.indexOf(transactionType) >= 0) {
-            log(`Received update for transaction ${transactionType}`);
-            log(`TX ${JSON.stringify(tx.transaction)}`);
+        client.on("transaction", async tx => {
+            const transactionType = tx?.transaction?.TransactionType;
+            if (transactionType != null && ValidTransactions.indexOf(transactionType) >= 0) {
+                log(`Received update for transaction ${transactionType}`);
+                log(`TX ${JSON.stringify(tx.transaction)}`);
 
-            switch (transactionType) {
-                case "NFTokenMint":
-                    await tokenMintHandler(tx, twitterClient);
-                    break;
-                case "NFTokenCreateOffer":
-                    await tokenCreateOfferHandler(tx, twitterClient);
-                    break;
-                case "NFTokenAcceptOffer":
-                    await tokenAcceptOfferHandler(tx, twitterClient);
-                    break;
+                switch (transactionType) {
+                    case "NFTokenMint":
+                        await tokenMintHandler(tx, twitterClient);
+                        break;
+                    case "NFTokenCreateOffer":
+                        await tokenCreateOfferHandler(tx, twitterClient);
+                        break;
+                    case "NFTokenAcceptOffer":
+                        await tokenAcceptOfferHandler(tx, twitterClient);
+                        break;
+                }
+
+                log(`Processed update for transaction ${transactionType}`);
             }
-
-            log(`Processed update for transaction ${transactionType}`);
+        });
+    }
+    catch (err) {
+        if (intervalTimer != null) {
+            clearInterval(intervalTimer);
         }
-    });
+        throw err;
+    }
 };
